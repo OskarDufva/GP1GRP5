3b7604a6f7c2ab441a3049dec3f33d4f0ee75850
Oskar Dufva Sundström <oskar_s_9@hotmail.com>
2022-10-19 10:34:04 +0200

    Scripts
    Added Wavespawner, wave, playerstats and gamemanager script.
    
    Added some more structure to the project.

---
+++ Assets/Scripts/Enemy/Wave.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave : MonoBehaviour
+{
+    public GameObject enemy;
+    public int count;
+    public float rate;
+}
